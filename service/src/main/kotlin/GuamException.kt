242607931b619141ca4db2346205d6986dfb9dd3
Jhvictor4 <wlgur7238@snu.ac.kr>
2021-11-21 06:15:02 +0900

    Exception 구조화
    - GuamException 정의
    - 커스텀 익셉션 전부 GuamException으로 귀속

---
+++ service/src/main/kotlin/GuamException.kt
@@ -0,0 +1,13 @@
+import org.springframework.http.HttpStatus
+
+abstract class GuamException(message: String, val status: HttpStatus) : Exception(message)
+
+open class GuamBadRequest(message: String, status: HttpStatus = HttpStatus.BAD_REQUEST) : GuamException(message, status)
+
+open class GuamUnAuthorized(message: String, status: HttpStatus = HttpStatus.UNAUTHORIZED) : GuamException(message, status)
+
+open class GuamForbidden(message: String, status: HttpStatus = HttpStatus.FORBIDDEN) : GuamException(message, status)
+
+open class GuamNotFound(message: String, status: HttpStatus = HttpStatus.NOT_FOUND) : GuamException(message, status)
+
+open class GuamConflict(message: String, status: HttpStatus = HttpStatus.CONFLICT) : GuamException(message, status)
