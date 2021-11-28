85fe80eb7b401259b63f4970aded17e2f8050fc8
강지혁 <wlgur7238@snu.ac.kr>
2021-11-28 17:42:36 +0900

    fix import settings

--- api/src/main/kotlin/config/ExceptionHandler.kt
+++ api/src/main/kotlin/config/ExceptionHandler.kt
@@ -1,12 +1,12 @@
 package waffle.guam.community.config
 
-import GuamException
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import waffle.guam.community.service.GuamException
 
 @ControllerAdvice
 class ErrorHandler {
