fdebccdba0a82850e389a800e1c6c8270ac7b5aa
강지혁 <wlgur7238@snu.ac.kr>
2021-11-28 17:42:56 +0900

    swagger config

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,4 +3,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux") // MVC 에서도 일단 사용하는 걸로..
     implementation("com.google.firebase:firebase-admin:7.1.0")
+    implementation("io.springfox:springfox-swagger2:2.9.2")
+    implementation("io.springfox:springfox-swagger-ui:2.9.2")
 }
