cb6a051bdd8dff395a82c1c9d52442cecd527497
jon <lgock2023@gmail.com>
2021-11-29 17:22:14 +0900

    [GCS-160] refactor: rename classes

--- service/src/main/kotlin/query/post/PostListCollector.kt
+++ service/src/main/kotlin/query/post/PostListCollector.kt
@@ -13,18 +13,18 @@ import waffle.guam.community.service.query.Collector
 @Service
 class PostListCollector(
     private val postRepository: PostRepository,
-) : Collector<PostList, PostListQuery>, PostQueryGenerator {
-    override fun get(id: PostListQuery): PostList {
+) : Collector<PostList, PostListCollector.Query>, PostQueryGenerator {
+    override fun get(id: Query): PostList {
         val spec = boardId(id.boardId) * status(PostEntity.Status.VALID) * afterPostId(id.afterPostId)
         val pageable = PageRequest.of(0, id.size, Sort.by(Sort.Direction.DESC, PostEntity_.ID))
 
         return postRepository.findAll(spec, pageable)
             .let { PostList.of(it) }
     }
-}
 
-data class PostListQuery(
-    val boardId: Long,
-    val afterPostId: Long,
-    val size: Int,
-)
+    data class Query(
+        val boardId: Long,
+        val afterPostId: Long,
+        val size: Int,
+    )
+}
