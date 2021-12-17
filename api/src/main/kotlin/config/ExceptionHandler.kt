3a6aa6b9a4e3d6802b68c793f440a0a3d756cf28
강지혁 <wlgur7238@snu.ac.kr>
2021-12-18 03:25:04 +0900

    Exception Handling 간소화
    Unhandled 500 처리 하지 말자

--- api/src/main/kotlin/config/ExceptionHandler.kt
+++ api/src/main/kotlin/config/ExceptionHandler.kt
@@ -1,28 +1,16 @@
 package waffle.guam.community.config
 
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import waffle.guam.community.service.GuamException
 
 @ControllerAdvice
-class ErrorHandler {
+class ExceptionHandler {
     private val logger = LoggerFactory.getLogger(this::javaClass.name)
-
-    @ExceptionHandler(value = [HttpRequestMethodNotSupportedException::class])
-    fun methodNotAllowed(e: HttpRequestMethodNotSupportedException) =
-        ResponseEntity(e.message, HttpStatus.METHOD_NOT_ALLOWED)
 
     @ExceptionHandler(value = [GuamException::class])
     fun guamError(e: GuamException) =
         ResponseEntity(e.message, e.status)
-
-    @ExceptionHandler(value = [Exception::class])
-    fun internalError(e: Exception) =
-        ResponseEntity("알 수 없는 문제가 발생하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR).also {
-            logger.error(e.message, e)
-        }
 }
