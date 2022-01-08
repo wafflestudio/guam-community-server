7d4386930899cfbeb27f21095b2fd7f9093350f2
강지혁 <wlgur7238@snu.ac.kr>
2022-01-08 17:14:59 +0900

    Swagger 플러그인 변경

--- api/src/main/kotlin/config/SwaggerConfig.kt
+++ api/src/main/kotlin/config/SwaggerConfig.kt
@@ -1,34 +1,48 @@
 package waffle.guam.community.config
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.models.parameters.Parameter
+import org.springdoc.core.GroupedOpenApi
+import org.springdoc.core.SpringDocUtils
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import springfox.documentation.builders.ApiInfoBuilder
-import springfox.documentation.builders.PathSelectors
-import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.service.ApiInfo
-import springfox.documentation.spi.DocumentationType
-import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2
+import waffle.guam.community.common.UserContext
 
+@OpenAPIDefinition(
+    info = Info(
+        title = "GCS API",
+        description = "Guam Community Server API"
+    )
+)
 @Configuration
-@EnableSwagger2
-class Swagger2Config {
-    @Bean
-    fun api(): Docket {
-        return Docket(DocumentationType.SWAGGER_2)
-            .select()
-            .apis(RequestHandlerSelectors.basePackage("waffle.guam.community.controller"))
-            .paths(PathSelectors.regex("/.*"))
-            .build()
-            .apiInfo(apiInfo())
-    }
+class SwaggerConfig {
 
-    private fun apiInfo(): ApiInfo {
-        return ApiInfoBuilder().apply {
-            title("Guam-Community API documentation")
-            description("API Endpoints")
-            license("WaffleStudio")
-            licenseUrl("https://github.com/wafflestudio/guam-community-server")
-        }.build()
+    @Bean
+    fun api(): GroupedOpenApi {
+        SpringDocUtils
+            .getConfig()
+            .addRequestWrapperToIgnore(UserContext::class.java) // Hide UserContext
+
+        return GroupedOpenApi
+            .builder()
+            .group("guam")
+            .pathsToMatch("/**")
+            .addOpenApiCustomiser { openApi ->
+                // auth 관련 API 빼고 Operation (API)에 인증 헤더를 추가해서 보여준다.
+                // https://github.com/springdoc/springdoc-openapi/issues/708
+                openApi.paths
+                    .filterNot { (pathName, _) -> pathName.contains("/auth") }
+                    .flatMap { (_, pathItem) -> pathItem.readOperations() }
+                    .forEach { operation ->
+                        operation.addParametersItem(
+                            Parameter()
+                                .`in`("header")
+                                .name("Authorization")
+                                .required(true)
+                        )
+                    }
+            }
+            .build()
     }
 }
