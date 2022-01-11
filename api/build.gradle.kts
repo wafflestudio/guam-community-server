a017757f8632a15856cda56f3a6e351bcb385d75
강지혁 <wlgur7238@snu.ac.kr>
2022-01-11 12:14:42 +0900

    슬랙 의존성 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 dependencies {
     implementation(project(":service"))
+    implementation(project(":slack"))
     implementation(project(":utils"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux") // MVC 에서도 일단 사용하는 걸로..
