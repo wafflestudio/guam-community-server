2d0c47a8333c0bd05acf6a43a52bd53010cf741d
강지혁 <wlgur7238@snu.ac.kr>
2022-01-15 16:08:56 +0900

    UrlConstants 삭제

--- community/api/src/main/kotlin/controller/user/UserController.kt
+++ community/api/src/main/kotlin/controller/user/UserController.kt
@@ -10,11 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import waffle.guam.community.common.UserContext
-import waffle.guam.community.controller.user.UserUriConstants.USER_COMMENTS
-import waffle.guam.community.controller.user.UserUriConstants.USER_DETAIL
-import waffle.guam.community.controller.user.UserUriConstants.USER_ME
-import waffle.guam.community.controller.user.UserUriConstants.USER_POSTS
-import waffle.guam.community.controller.user.UserUriConstants.USER_STACK
 import waffle.guam.community.controller.user.req.AddUserStackRequest
 import waffle.guam.community.controller.user.req.UpdateUserRequest
 import waffle.guam.community.service.command.user.UpdateUserHandler
@@ -24,7 +19,7 @@ import waffle.guam.community.service.command.user.stack.DeleteUserStackHandler
 import waffle.guam.community.service.query.post.displayer.PostDisplayer
 import waffle.guam.community.service.query.user.displayer.UserDisplayer
 
-@RequestMapping
+@RequestMapping("api/v1/users")
 @RestController
 class UserController(
     private val updateUserHandler: UpdateUserHandler,
@@ -33,39 +28,39 @@ class UserController(
     private val userDisplay: UserDisplayer,
     private val postDisplay: PostDisplayer,
 ) {
-    @GetMapping(USER_ME)
+    @GetMapping("/me")
     fun userMe(
         userContext: UserContext,
     ) = userDisplay.getUser(userContext.id)
 
-    @GetMapping(USER_DETAIL)
+    @GetMapping("/{userId}")
     fun userDetail(
         userContext: UserContext,
         @PathVariable userId: Long,
     ) = userDisplay.getUser(userId)
 
-    @PatchMapping(USER_DETAIL)
+    @PatchMapping("/{userId}")
     fun userUpdate(
         userContext: UserContext,
         @PathVariable userId: Long,
         @RequestBody request: UpdateUserRequest,
     ) = updateUserHandler.handle(request.toCommand(userId))
 
-    @PostMapping(USER_STACK)
+    @PostMapping("{userId}/stack")
     fun addStack(
         userContext: UserContext,
         @PathVariable userId: Long,
         @RequestBody request: AddUserStackRequest,
     ) = createUserStackHandler.handle(request.toCommand(userId))
 
-    @DeleteMapping(USER_STACK)
+    @DeleteMapping("{userId}/posts")
     fun addStack(
         userContext: UserContext,
         @PathVariable userId: Long,
         @RequestParam(required = true) name: String,
     ) = deleteUserStackHandler.handle(DeleteUserStack(userId, name))
 
-    @GetMapping(USER_POSTS)
+    @GetMapping("{userId}/comments")
     fun userPosts(
         userContext: UserContext,
         @PathVariable userId: Long,
@@ -73,7 +68,7 @@ class UserController(
         @RequestParam(required = false) sortByLikes: Boolean?,
     ) = postDisplay.getUserPostList(userId, afterPostId, sortByLikes ?: false)
 
-    @GetMapping(USER_COMMENTS)
+    @GetMapping("{userId}/comments")
     fun userComments(
         userContext: UserContext,
         @PathVariable userId: Long,
