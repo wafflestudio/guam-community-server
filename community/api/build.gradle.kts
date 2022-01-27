12a1d7b6c188e369ae58432b7b97ff4a4f5561d7
jon <lgock2023@gmail.com>
2022-01-27 21:12:38 +0900

    fix: wrong import

--- community/api/build.gradle.kts
+++ community/api/build.gradle.kts
@@ -7,7 +7,6 @@ dependencies {
     implementation(project(":community::service"))
     implementation(project(":community::slack"))
     implementation(project(":community::utils"))
-    implementation(project(":immigration:client"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux") // MVC 에서도 일단 사용하는 걸로..
     implementation("com.google.firebase:firebase-admin:7.1.0")
