72ac48af07a7bb7e41b9bdb10b67780062b8918a
jon <lgock2023@gmail.com>
2022-03-01 01:53:25 +0900

    fix: users primary strategy

--- data/data-jdbc/src/main/kotlin/user/UserEntity.kt
+++ data/data-jdbc/src/main/kotlin/user/UserEntity.kt
@@ -7,8 +7,6 @@ import javax.persistence.Column
 import javax.persistence.Convert
 import javax.persistence.Entity
 import javax.persistence.FetchType
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.OneToMany
 import javax.persistence.Table
@@ -16,7 +14,6 @@ import javax.persistence.Table
 @Table(name = "users")
 @Entity
 class UserEntity(
-    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     val id: Long, // same as immigration service ID
 
