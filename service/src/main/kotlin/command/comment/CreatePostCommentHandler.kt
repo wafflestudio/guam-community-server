2bd21d07b28af233b7994e3949415ab183e9094d
강지혁 <wlgur7238@snu.ac.kr>
2022-03-20 16:24:35 +0900

    feat: comment image upload

--- service/src/main/kotlin/command/comment/CreatePostCommentHandler.kt
+++ service/src/main/kotlin/command/comment/CreatePostCommentHandler.kt
@@ -3,6 +3,7 @@ package waffle.guam.community.service.command.comment
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.multipart.MultipartFile
 import waffle.guam.community.common.PostNotFound
 import waffle.guam.community.common.UserNotFound
 import waffle.guam.community.data.jdbc.comment.PostCommentEntity
@@ -14,21 +15,25 @@ import waffle.guam.community.service.UserId
 import waffle.guam.community.service.command.Command
 import waffle.guam.community.service.command.CommandHandler
 import waffle.guam.community.service.command.Result
+import waffle.guam.community.service.command.image.UploadImageList
+import waffle.guam.community.service.command.image.UploadImageListHandler
+import waffle.guam.community.service.domain.image.ImageType
 
 @Service
 class CreatePostCommentHandler(
     private val postRepository: PostRepository,
     private val userRepository: UserRepository,
+    private val uploadImageListHandler: UploadImageListHandler,
 ) : CommandHandler<CreatePostComment, PostCommentCreated> {
 
     @Transactional
     override fun handle(command: CreatePostComment): PostCommentCreated {
-        val (postId, userId, content) = command
+        val (postId, userId, content, images) = command
 
         val post = postRepository.findByIdOrNull(postId) ?: throw PostNotFound(postId)
         val user = userRepository.findByIdOrNull(userId) ?: throw UserNotFound(userId)
 
-        post.addCommentBy(user, content)
+        post.addCommentBy(user, content, images)
 
         return PostCommentCreated(
             postId = postId,
@@ -40,9 +45,15 @@ class CreatePostCommentHandler(
         )
     }
 
-    private fun PostEntity.addCommentBy(user: UserEntity, content: String) {
+    private fun PostEntity.addCommentBy(user: UserEntity, content: String, images: List<MultipartFile>) {
+        val newComment = PostCommentEntity(post = this, user = user, content = content)
+        comments.add(newComment.apply { addImages(images) })
+    }
 
-        comments.add(PostCommentEntity(post = this, user = user, content = content))
+    // TODO: rollback uploaded image on error
+    private fun PostCommentEntity.addImages(imageList: List<MultipartFile>) {
+        val uploadImageRequest = UploadImageList(parentId = id, type = ImageType.POST, images = imageList)
+        images.addAll(uploadImageListHandler.handle(uploadImageRequest).imagePaths)
     }
 }
 
@@ -50,6 +61,7 @@ data class CreatePostComment(
     val postId: Long,
     val userId: Long,
     val content: String,
+    val images: List<MultipartFile>,
     val mentionIds: List<UserId>,
 ) : Command
 
