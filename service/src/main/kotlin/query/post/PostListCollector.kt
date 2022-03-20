735aef153d5a90375ffdae0729ca8b5e4b9e2526
강지혁 <wlgur7238@snu.ac.kr>
2022-03-20 19:15:17 +0900

    fix: uncaught recent posts method

--- service/src/main/kotlin/query/post/PostListCollector.kt
+++ service/src/main/kotlin/query/post/PostListCollector.kt
@@ -15,7 +15,8 @@ class PostListCollector(
     private val postRepository: PostRepository,
 ) : Collector<PostList, PostListCollector.Query>, PostQueryGenerator {
     override fun get(id: Query): PostList {
-        val spec = boardId(id.boardId) * status(PostEntity.Status.VALID) * beforePostId(id.beforePostId)
+        val baseSpec = boardId(id.boardId) * status(PostEntity.Status.VALID)
+        val spec = if (id.beforePostId == null) baseSpec else baseSpec * beforePostId(id.beforePostId)
         val pageable = PageRequest.of(id.page, id.size, Sort.by(Sort.Direction.DESC, PostEntity_.ID))
 
         return PostList(postRepository.findAll(spec, pageable))
@@ -23,7 +24,7 @@ class PostListCollector(
 
     data class Query(
         val boardId: Long?,
-        val beforePostId: Long = 0,
+        val beforePostId: Long? = null,
         val page: Int = 0,
         val size: Int = 20,
     )
