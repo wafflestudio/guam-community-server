9bf9ad181a5419d942f6486df88c57e09074e73b
강지혁 <wlgur7238@snu.ac.kr>
2022-03-20 19:15:17 +0900

    fix: uncaught recent posts method
    + nullable createPostRequest

--- api/src/main/kotlin/controller/comment/req/CreatePostCommentRequest.kt
+++ api/src/main/kotlin/controller/comment/req/CreatePostCommentRequest.kt
@@ -6,14 +6,14 @@ import waffle.guam.community.service.command.comment.CreatePostComment
 
 data class CreatePostCommentRequest(
     val content: String,
-    val images: List<MultipartFile>,
     val mentionIds: List<UserId>,
+    val images: List<MultipartFile>?,
 ) {
     fun toCommand(postId: Long, userId: Long): CreatePostComment = CreatePostComment(
         postId = postId,
         userId = userId,
         content = content,
-        images = images,
+        images = images ?: listOf(),
         mentionIds = mentionIds
     )
 }
