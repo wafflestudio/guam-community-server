b58eb749fab7bd007a2298e06aaebb59f3f95e2a
강지혁 <wlgur7238@snu.ac.kr>
2022-05-07 11:20:16 +0900

    feat: PushEventEntity 익명 여부 추가

--- service/src/main/kotlin/command/notification/CreatePushEventHandler.kt
+++ service/src/main/kotlin/command/notification/CreatePushEventHandler.kt
@@ -2,7 +2,6 @@ package waffle.guam.community.service.command.notification
 
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Service
-import waffle.guam.community.data.jdbc.push.PushEventEntity
 import waffle.guam.community.data.jdbc.push.PushEventRepository
 import waffle.guam.community.data.jdbc.user.UserRepository
 import waffle.guam.community.service.command.comment.PostCommentCreated
@@ -14,32 +13,9 @@ class CreatePushEventHandler(
 ) {
     @EventListener
     fun postCommentCreated(event: PostCommentCreated) {
-        val writer = userRepository.findById(event.writerId).get()
-
-        pushEventRepository.save(
-            PushEventEntity(
-                userId = event.postUserId,
-                writer = writer,
-                kind = PushEventEntity.Kind.POST_COMMENT,
-                body = event.content,
-                linkUrl = "/api/v1/posts/${event.postId}",
-                isRead = false
-            )
-        )
-
-        if (event.mentionIds.isNotEmpty()) {
-            val mentionedEvents = event.mentionIds.map {
-                PushEventEntity(
-                    userId = it,
-                    writer = writer,
-                    kind = PushEventEntity.Kind.POST_COMMENT_MENTION,
-                    body = event.content,
-                    linkUrl = "/api/v1/posts/${event.postId}",
-                    isRead = false
-                )
-            }
-
-            pushEventRepository.saveAll(mentionedEvents)
-        }
+        val writer = userRepository.getById(event.writerId)
+        val postCommentCreatedEvent = event.toCreatedEventEntity(writer)
+        val mentionedEvents = event.toMentionEventEntity(writer)
+        pushEventRepository.saveAll(mentionedEvents + postCommentCreatedEvent)
     }
 }
