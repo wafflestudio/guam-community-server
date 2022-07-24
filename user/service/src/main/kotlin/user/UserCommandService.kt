970066dae7c83b095509722e904722ef3a3b7919
강지혁 <wlgur7238@snu.ac.kr>
2022-07-24 10:34:53 +0900

    [User] constraint & exception handling

--- user/service/src/main/kotlin/user/UserCommandService.kt
+++ user/service/src/main/kotlin/user/UserCommandService.kt
@@ -13,6 +13,7 @@ import waffle.guam.user.service.UserNotFound
 import waffle.guam.user.service.user.UserCommandService.CreateInterest
 import waffle.guam.user.service.user.UserCommandService.DeleteInterest
 import waffle.guam.user.service.user.UserCommandService.UpdateUser
+import javax.validation.constraints.Min
 
 interface UserCommandService : CommandService {
     fun updateUser(command: UpdateUser): User
@@ -21,6 +22,7 @@ interface UserCommandService : CommandService {
 
     data class UpdateUser(
         val userId: Long,
+        @field:Min(value = 2, message = "Nickname should be greater than 1.")
         val nickname: String?,
         val introduction: String?,
         val githubId: String?,
