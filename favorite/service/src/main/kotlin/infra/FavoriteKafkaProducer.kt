7d6d78cb4903c28fe1944a2c985b74431ec9cbff
jon <lgock2023@gmail.com>
2022-08-03 15:59:00 +0900

    fix kafka broker ip

--- favorite/service/src/main/kotlin/infra/FavoriteKafkaProducer.kt
+++ favorite/service/src/main/kotlin/infra/FavoriteKafkaProducer.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import kotlinx.coroutines.reactor.awaitSingle
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
 import org.springframework.stereotype.Service
@@ -18,13 +19,17 @@ interface FavoriteKafkaProducer {
 }
 
 // TODO: property 주입
+// TODO: timeout, error handle or recover
+// TODO: transcational 밖으로 빼고 싶다.
 @Service
 class FavoriteKafkaProducerImpl : FavoriteKafkaProducer {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     private val objectMapper = jacksonObjectMapper().also { it.registerModule(JavaTimeModule()) }
     private val kafkaProducer = ReactiveKafkaProducerTemplate<String, String>(
         SenderOptions.create(
             KafkaProperties().apply {
-                this.bootstrapServers = listOf("34.64.147.36:30182")
+                this.bootstrapServers = listOf("34.64.147.36:9092")
                 this.clientId = "guam-favorite-dev"
             }
                 .buildProducerProperties()
@@ -40,6 +45,8 @@ class FavoriteKafkaProducerImpl : FavoriteKafkaProducer {
         }
         val message = objectMapper.writeValueAsString(event)
 
-        kafkaProducer.send(ProducerRecord(topic, message)).awaitSingle()
+        kafkaProducer.send(ProducerRecord(topic, message))
+            .map { if (it.exception() != null) logger.error("kafka produce failed", it.exception()) }
+            .awaitSingle()
     }
 }
