c138820ea61cb9fe06ac519989639933da2c44be
jon <lgock2023@gmail.com>
2022-10-03 12:41:21 +0900

    r2dbc-h2 testRuntimeOnly

--- letter/data/r2dbc/build.gradle.kts
+++ letter/data/r2dbc/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework:spring-context")
 
-    runtimeOnly("io.r2dbc:r2dbc-h2")
+    testRuntimeOnly("io.r2dbc:r2dbc-h2")
     runtimeOnly("dev.miku:r2dbc-mysql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-webflux")
